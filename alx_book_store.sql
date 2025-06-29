1d7b54bff6f38da1a057d96f596b27f04082f5b0
OLAJIRE <olajirefolasore@gmail.com>
2025-06-29 22:15:18 +0100

    add

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -5,7 +5,7 @@ USE alx_book_store;
 CREATE TABLE Books(
     book_id INT PRIMARY KEY,
     title VARCHAR(130),
-    author_id INT,
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id),
     price DOUBLE,
     publication_date DATE
 );
@@ -25,14 +25,14 @@ CREATE TABLE Customers(
 
 CREATE TABLE Orders(
     order_id INT PRIMARY KEY,
-    customer_id INT,
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id) ,
     order_date DATE
 
 );
 
 CREATE TABLE Order_Details(
     orderdetailid INT PRIMARY KEY,
-    order_id INT,
-    book_id INT,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id),
     quantity DOUBLE
 )
